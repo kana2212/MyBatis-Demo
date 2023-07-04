a297478bd46043a1783b18f12f0087433c7cd5c4
kana2212 <cikiruki@yahoo.co.jp>
2023-07-04 11:50:38 +0900

    applicationクラスの@Configurationを削除

--- src/main/java/com/example/MyBatisMember/MemberApplication.java
+++ src/main/java/com/example/MyBatisMember/MemberApplication.java
@@ -3,10 +3,8 @@ package com.example.MyBatisMember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class MemberApplication {
 
     public static void main(String[] args) {
